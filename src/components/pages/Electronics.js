095c13e55cc0f7a259bdedbf63c833e866ac154d
HJ Miranda <hjmiranda02@gmail.com>
2022-04-28 15:11:07 +0800

    text inserted

--- src/components/pages/Electronics.js
+++ src/components/pages/Electronics.js
@@ -2,8 +2,184 @@ import React from 'react'
 
 function Electronics() {
     return (
-        <div className="electronics global">
+        <div className="electronics">
+        <div className="title">
         Electronics Engineering
+        </div>
+
+        <div className="sub">
+            Program Educational Objectives (PEO)
+        </div>
+
+        <div className="list">
+            <ul>
+                <li>Graduates of electrical engineering program are experts in their field of work to cope with the needs and demands of the society they serve.</li>
+                <li>Three to five years after graduation they will contribute to the advancement of technology and industrialization</li>
+                <li>Their work as an electrical engineer imbued with Christian awareness, pride, honor and dignity. </li>
+                <li>The graduates are fully equipped with knowledge and experienced towards nation building</li>
+                <li>Contribute to the nation's development with sense of social responsibility and accountability, establishing values of honors, sense of urgency and love of work.</li>
+            </ul>
+        </div>
+
+        <div className="sub">
+        SKILLS
+        </div>
+
+        <div className="list">
+            <ul>
+               <li>Critical Thinking</li>
+               <li>Circuit Design</li>
+               <li>Instrumentation and Electrical Measurements.</li>
+               <li>Active Learning</li>
+               <li>Interpersonal Communication</li>
+               <li>Organizational Skills</li>
+               <li>Complex Problem Solving</li>
+               <li>Mathematics and Physics</li>
+               <li>Innovative Thinking</li>
+               <li>Programming Languages</li>
+               <li>Technological knowledge</li>
+            </ul>
+        </div>
+
+        <div className="sub">
+        JOB OPPORTUNITIES
+        </div>
+        <div className="mini-sub">
+        Job opportunities for Electrical Engineering Graduates:
+        </div>
+        <div className="list">
+            <ul>
+               <li>Electrical Technician</li>
+               <li>Electrician</li>
+               <li>Controls Engineer</li>
+               <li>Test Engineer</li>
+               <li>Electrical project manager</li>
+               <li>Electrical Designer</li>
+               <li>Sustainability Engineer</li>
+               <li>Electrical Engineer</li>
+               <li>Equipment Engineer</li>
+               <li>Aeronautical Engineer</li>
+               <li>Electronics Engineer</li>
+               <li>Hardware Engineer</li>
+               <li>Software Engineer</li>
+               <li>Director of Technology</li>
+               <li>Biotech Consultant</li>
+            </ul>
+        </div>
+
+        <div className="mini-sub">
+        Top careers for Electrical Engineers:
+        </div>
+
+        <div className="list">
+            <ul>
+               <li>Principal Electrical Engineern</li>
+               <li>Electrical Distribution Designer</li>
+               <li>Aerospace Electrical Engineer</li>
+               <li>Electrical Project Engineer</li>
+               <li>Electrical Control Engineer</li>
+            </ul>
+        </div>
+
+        <div className="sub">
+        BOARD EXAM RANKING AND PASSING PERCENTILE
+        </div>
+
+        <div className="sub">
+        2018
+        </div>
+
+        <div className="table">
+        <table cellspacing="10">
+            <tr>
+                <th>RANK</th>
+                <th>SCHOOL</th>
+                <th>TOTAL NO. OF EXAM.</th>
+                <th>TOTAL NO. PASSED</th>
+                <th>PERCENTAGE PASSED</th>
+            </tr>
+
+            <tr>
+                <td> </td>
+                <td>BULACAN STATE UNIVERSITY (FOR. BCAT)- MALOLOS</td>
+                <td>72</td>
+                <td>54</td>
+                <td>75.00%</td>
+            </tr>
+        </table>
+        </div>
+
+        <div className="sub">
+        2019
+        </div>
+
+        <div className="table">
+        <table cellspacing="10">
+            <tr>
+                <th>RANK</th>
+                <th>SCHOOL</th>
+                <th>TOTAL NO. OF EXAM.</th>
+                <th>TOTAL NO. PASSED</th>
+                <th>PERCENTAGE PASSED</th>
+            </tr>
+
+            <tr>
+                <td> </td>
+                <td>BULACAN STATE UNIVERSITY (FOR. BCAT)- MALOLOS</td>
+                <td>136</td>
+                <td>115</td>
+                <td>84.56%</td>
+            </tr>
+        </table>
+        </div>
+
+        <div className="sub">
+        2021
+        </div>
+
+        <div className="table">
+        <table cellspacing="10">
+            <tr>
+                <th>RANK</th>
+                <th>SCHOOL</th>
+                <th>TOTAL NO. OF EXAM.</th>
+                <th>TOTAL NO. PASSED</th>
+                <th>PERCENTAGE PASSED</th>
+            </tr>
+
+            <tr>
+                <td> </td>
+                <td>BULACAN STATE UNIVERSITY (FOR. BCAT)- MALOLOS</td>
+                <td>20</td>
+                <td>13</td>
+                <td>65.00%</td>
+            </tr>
+        </table>
+        </div>
+
+        <div className="sub">
+        TOPNOTCHERS
+        </div>
+        <div className="sub">
+        2018
+        </div>
+        <div className="mini-sub">
+        ENGR. CARLO GATBONTON BAUTISTA
+        </div>
+
+        <div className="sub">
+        2019
+        </div>
+        <div className="mini-sub">
+        ENGR. JOSE GABRIEL MADULID  DELOS SANTOS
+        </div>
+        <div className="mini-sub">
+        ENGR. RENZ CHRISTIAN DUANAN  DELOS SANTOS
+        </div>
+        <div className="mini-sub">
+        ENGR. KARLO AGUILAR  AVENDAÑO
+        </div>
+       
         </div>
     )
 }
